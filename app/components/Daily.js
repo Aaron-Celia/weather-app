414a503fa3dee3f1d6dcf3d4e046b4a3a25520a3
Aaron-Celia <card123er@gmail.com>
2023-06-20 16:59:15 -0400

    touch up

--- app/components/Daily.js
+++ app/components/Daily.js
@@ -22,7 +22,7 @@ export default function Daily() {
 			};
 
   return (
-      <TableContainer mt='10' borderRadius='2xl'>
+      <TableContainer mt='5' borderRadius='2xl'>
         <Table>
           <Thead sx={tableHeadStyle}>
             <Th>Day</Th>
